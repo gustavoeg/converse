c865fa7d0c1ac10c24af5c75d7cb314634680944
GustavoG <gustavo_eg81@hotmail.com>
2024-04-12 15:06:46 -0300

    menu agregado

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Simple chat bot</title>
+    <style>
+        body{
+            background-color: black;
+        }
+    </style>
 </head>
 <body>
     <h3>Botman demo</h3>
