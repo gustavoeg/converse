a39eb5f0299f51c5b528747d34cecdf57b812635
gustavo <gustavoeguanuco@gmail.com>
2024-04-18 22:51:46 -0300

    consulta api

--- chat.php
+++ chat.php
@@ -39,7 +39,18 @@ $botman->hears('clima en {location}', function ($bot,$location){
 });
 
 $botman->hears('wit.ai', function ($bot,$location){
-    $bot->reply('Ingresaste: ' . $location);
+    $user_consulta = "Crear el contacto gustavo";
+    $url = "https://api.wit.ai/message?v=20240304&q=" . $user_consulta;
+    $token = "42OKHWM7P7YSJV4QYAWKBILFDE5HV5LA";
+$options = array('http' => array(
+    'method'  => 'GET',
+    'header' => 'Authorization: Bearer '.$token
+));
+$context  = stream_context_create($options);
+$response = file_get_contents($url, false, $context);
+
+print_r($response);
+    $bot->reply('Ingresaste: ' . $response);
 });
 
 $botman->hears('survey', function ($bot){
