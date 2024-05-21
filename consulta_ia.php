afa44d986835fe5536351a251dd3e77fabd9e7eb
GustavoG <gustavo_eg81@hotmail.com>
2024-05-21 15:35:46 -0300

    consulta a wit.ai

--- consulta_ia.php
+++ consulta_ia.php
@@ -8,7 +8,8 @@ function preguntar_API_IA($pregunta){
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
     $dotenv->load();
 
-    $user_consulta = urlencode("borra el contacto Samuel");
+    //$user_consulta = urlencode("borra el contacto Samuel");
+    $user_consulta = urlencode($pregunta);
     $url = "https://api.wit.ai/message?v=20240304&q=" . $user_consulta;
     $token = $_ENV['TOKEN_WITAI'];
     $options = array('http' => array(
@@ -20,12 +21,12 @@ function preguntar_API_IA($pregunta){
         $response = file_get_contents($url, false, $context);
         $resultado = json_decode($response,true); //respuesta procesada
         //print_r($resultado);
-
+ 
         //echo "<br>ACCION: ";
-        if(count($resultado['entities']['tsj_action:tsj_action']) > 0){
-            $accion_nombre = $resultado['entities']['tsj_action:tsj_action'][0]['value'];
-            $accion_confianza = $resultado['entities']['tsj_action:tsj_action'][0]['confidence'];
-            $accion = array('accion_nombre' => $accion_nombre, 'accion_confianza' => $accion_confianza);
+        if(count($resultado['entities']['tsj_direccion:tsj_direccion']) > 0){
+            $accion_nombre = $resultado['entities']['tsj_direccion:tsj_direccion'][0]['value'];
+            $accion_confianza = $resultado['entities']['tsj_direccion:tsj_direccion'][0]['confidence'];
+            $accion = array('direccion' => $accion_nombre, 'accion_confianza' => $accion_confianza);
         }else{
             $accion_nombre = "";
             $accion_confianza = "";
@@ -34,11 +35,23 @@ function preguntar_API_IA($pregunta){
         
 
         //echo "<br>OBJETO: ";
-        if(count($resultado['entities']['tsj_object:tsj_object']) > 0){
+        if(count($resultado['entities']['tsj_dependencia:tsj_dependencia']) > 0){
             //
-            $objeto_nombre = $resultado['entities']['tsj_action:tsj_action'][0]['value'];
-            $objeto_confianza = $resultado['entities']['tsj_action:tsj_action'][0]['confidence'];
-            $objeto = array('objeto_nombre' => $objeto_nombre, 'objeto_confianza' => $objeto_confianza);
+            $objeto_nombre = $resultado['entities']['tsj_dependencia:tsj_dependencia'][0]['value'];
+            $objeto_confianza = $resultado['entities']['tsj_dependencia:tsj_dependencia'][0]['confidence'];
+            $objeto = array('dependencia nombre' => $objeto_nombre, 'objeto_confianza' => $objeto_confianza);
+        }else{
+            $objeto_nombre = "";
+            $objeto_confianza = "";
+            $objeto = "";
+        }
+
+        //echo "<br>Localidad: ";
+        if(count($resultado['entities']['tsj_localidad:tsj_localidad']) > 0){
+            //
+            $objeto_nombre = $resultado['entities']['tsj_localidad:tsj_localidad'][0]['value'];
+            $objeto_confianza = $resultado['entities']['tsj_localidad:tsj_localidad'][0]['confidence'];
+            $objeto = array('localidad_nombre' => $objeto_nombre, 'objeto_confianza' => $objeto_confianza);
         }else{
             $objeto_nombre = "";
             $objeto_confianza = "";
@@ -46,7 +59,7 @@ function preguntar_API_IA($pregunta){
         }
         
 
-        //echo "<br>CUERPO: ";
+        /* //echo "<br>CUERPO: ";
         if(count($resultado['entities']['wit$message_body:message_body']) > 0){
             $cuerpo_nombre = $resultado['entities']['wit$message_body:message_body'][0]['value'];
             $cuerpo_confianza = $resultado['entities']['wit$message_body:message_body'][0]['confidence'];
@@ -55,9 +68,9 @@ function preguntar_API_IA($pregunta){
             $cuerpo_nombre = "";
             $cuerpo_confianza = "";
             $cuerpo = "";
-        }
+        } */
         
-
+ 
         if (count($resultado['intents']) > 0) {
             $intencion_nombre = $resultado['intents'][0]['name'];
             $intencion_confianza = $resultado['intents'][0]['confidence'];
@@ -83,7 +96,7 @@ function preguntar_API_IA($pregunta){
         $para_enviar = array(
             'accion' => $accion,
             'objeto' => $objeto,
-            'cuerpo' => $cuerpo,
+            //'cuerpo' => $cuerpo,
             'intencion' => $intencion,
             'trait' => $trait,
             'error' => false);
--- sample2.php
+++ sample2.php
@@ -6,7 +6,8 @@ require './datos/DependenciaDAO.php';
 require './datos/ConexionDB.php';
 require 'consulta_ia.php';
 
-try {
+if(isset($_POST['pregunta_enviada']) ){
+/* try {
     $instance = ConexionDB::getInstance();
     // Crear una instancia del DAO
     $dependenciaDAO = new DependenciaDAO($instance->getConexion());
@@ -43,8 +44,18 @@ try {
 } catch (\PDOException $e) {
     echo "Error de conexión: " . $e->getMessage();
 }
-
-$pregunta = "por favor borra el contacto omar";
-echo "Consulta a API IA:";
+ */
+//$pregunta = "por favor borra el contacto omar";
+$pregunta = $_POST['pregunta'];
+//echo "Consulta a API IA:";
 $respuesta_api = preguntar_API_IA($pregunta);
 print_r($respuesta_api);
+}else{
+    ?>
+    <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
+        <label for="pregunta">Pregunta a wit.ai</label>
+        <input type="text" name="pregunta" id="pregunta">
+        <input type="submit" name="pregunta_enviada" value="pregunta_enviada">
+    </form>
+    <?php
+}
